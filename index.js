eb3ad79a30adf8090f5a6888f145eac096f4738e
Jcistre <josephcistre@gmail.com>
2025-04-17 13:46:50 -0500

    done

--- index.js
+++ index.js
@@ -71,37 +71,71 @@ const purchases = [
 
 // #1 // 
 const getNumberEntries = (object) => {
-  
+  let output = []
+  for (let key in object) {
+    if (typeof object[key] === 'number') {
+      output.push([key, object[key]])
+    }
+  }
+  return output
 };
 
 // #2 // 
 const addKeyValuePairs = (object, additions) => {
-  
+  for (let i = 0; i < additions.length; i++) {
+    object[additions[i][0]] = additions[i][1]
+  }
+  return object
 };
 
 // #3 //
 const filterByPrice = (array, price) => {
-
+  return array.filter(function(obj) {
+    return obj.price > price
+  })
 };
 
 // #4 //
 const mapPurchases = (array) => {
-  
+  return array.map(function(obj) {
+    let latest = obj.mostLikedReviews.length - 1
+    return `${obj.product.toUpperCase()} - Review: ${obj.mostLikedReviews[latest].text}`
+  })
 };
 
 // #5 //
 const accumulateString = (array) => {
-  
+  return array.reduce(function(accStr, obj) {
+    let array2 = obj.product.split(" ")
+    accStr += array2[obj.quantity - 1]
+    return accStr
+  }, "")
 };
 
 // #6 //
-const findProduct = (array, product) => {
-  
+const findProduct = (array, product, output = []) => {
+  if (array.length === 0 && output.length !== 0) {
+    return output
+  } else if (array.length === 0 && output.length === 0) {
+    return [null, null]
+  }
+  if (array[0].product === product) {
+    output.push(array[0].product);
+    output.push(array[0].category);
+  }
+  return findProduct(array.slice(1), product, output)
 };
 
 // #7 //
 const filterByReviewLength = (array) => {
-  
+  return array.filter(function(obj) {
+    let reviews = obj.mostLikedReviews
+    for (let i = 0; i < reviews.length; i++) {
+      if (reviews[i].text.length > 35) {
+        return obj.product
+      }
+    }
+  })
 };
 
 
